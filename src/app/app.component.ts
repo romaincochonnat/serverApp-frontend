08baa01a666f6b71bece3aca90710ad7b5ff3cb7
Romain Cochonnat <romain.cochonnat@gmail.com>
2024-11-01 20:42:00 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,194 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ServerService } from './service/server.service';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import {
+  BehaviorSubject,
+  catchError,
+  map,
+  Observable,
+  of,
+  startWith,
+} from 'rxjs';
+import { DataState } from './enum/data-state.enum';
+import { Status } from './enum/status.enum';
+import { NgForm } from '@angular/forms';
+import { Server } from './interface/server';
+import { NotificationService } from './service/notification.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'serverapp';
+export class AppComponent implements OnInit {
+  appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+  readonly Status = Status;
+  private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
+  filterStatus$ = this.filterSubject.asObservable();
+  private isLoading = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.isLoading.asObservable();
+
+
+  constructor(
+    private serverService: ServerService,
+    private notifier: NotificationService
+  ) {}
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.getServer$().pipe(
+      map((response) => {
+        this.notifier.onSuccess(response.message)
+        this.dataSubject.next(response);
+        return {
+          dataState: DataState.LOADED_STATE,
+          appData: {
+            ...response,
+            data: { servers: response.data.servers.reverse() },
+          },
+        };
+      }),
+      startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) => {
+        this.notifier.onError(error)
+        return of({ dataState: DataState.ERROR_STATE, error });
+      })
+    );
+  }
+
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress);
+    this.appState$ = this.serverService.ping$(ipAddress).pipe(
+      map((response) => {
+        const index = this.dataSubject.value.data.servers.findIndex(
+          (server) => server.id === response.data.server.id
+        );
+        this.dataSubject.value.data.servers[index] = response.data.server;
+        this.notifier.onDefault(response.message)
+        this.filterSubject.next('');
+        return {
+          dataState: DataState.LOADED_STATE,
+          appData: this.dataSubject.value,
+        };
+      }),
+      startWith({
+        dataState: DataState.LOADED_STATE,
+        appData: this.dataSubject.value,
+      }),
+      catchError((error: string) => {
+        this.filterSubject.next('');
+        this.notifier.onError(error)
+        return of({ dataState: DataState.ERROR_STATE, error });
+      })
+    );
+  }
+
+  onStatusChange(event: Event): void {
+    const selectedStatus = (event.target as HTMLSelectElement).value as Status;
+    this.filterServers(selectedStatus);
+  }
+
+  filterServers(status: string): void {
+    const statusEnum = status as Status; // Conversion en type Status
+    this.appState$ = this.serverService
+      .filter$(statusEnum, this.dataSubject.value)
+      .pipe(
+        map((response) => {
+          this.notifier.onDefault(response.message)
+          return { dataState: DataState.LOADED_STATE, appData: response };
+        }),
+        startWith({
+          dataState: DataState.LOADED_STATE,
+          appData: this.dataSubject.value,
+        }),
+        catchError((error: string) => {
+          this.notifier.onError(error)
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
+  }
+
+  saveServer(serverForm: NgForm): void {
+    this.isLoading.next(true);
+    this.appState$ = this.serverService.save$(serverForm.value as Server).pipe(
+      map((response) => {
+        this.notifier.onSuccess(response.message)
+        this.dataSubject.next({
+          ...response,
+          data: {
+            servers: [
+              response.data.server,
+              ...this.dataSubject.value.data.servers,
+            ],
+          },
+        });
+        document.getElementById('closeModal').click();
+        this.isLoading.next(false);
+        serverForm.resetForm({ status: this.Status.SERVER_DOWN });
+        return {
+          dataState: DataState.LOADED_STATE,
+          appData: this.dataSubject.value,
+        };
+      }),
+      startWith({
+        dataState: DataState.LOADED_STATE,
+        appData: this.dataSubject.value,
+      }),
+      catchError((error: string) => {
+        this.notifier.onError(error)
+        this.isLoading.next(false);
+        return of({ dataState: DataState.ERROR_STATE, error });
+      })
+    );
+  }
+
+  deleteServer(server: Server): void {
+    this.appState$ = this.serverService.delete$(server.id).pipe(
+      map((response) => {
+        this.notifier.onDefault(response.message)
+        this.dataSubject.next({
+          ...response,
+          data: {
+            servers: this.dataSubject.value.data.servers.filter(
+              (s) => s.id !== server.id
+            ),
+          },
+        });
+        return {
+          dataState: DataState.LOADED_STATE,
+          appData: this.dataSubject.value,
+        };
+      }),
+      startWith({
+        dataState: DataState.LOADED_STATE,
+        appData: this.dataSubject.value,
+      }),
+      catchError((error: string) => {
+        this.notifier.onError(error)
+        this.filterSubject.next('');
+        return of({ dataState: DataState.ERROR_STATE, error });
+      })
+    );
+  }
+
+  printReport(): void {
+    this.notifier.onDefault("Report downloaded")
+    // window.print(); //pour permettre l'enregistrement en pdf de la page
+    let dataType = 'application/vnd.ms-excel';
+    let tableSelect = document.getElementById('servers');
+    if (tableSelect) {
+      let tableHtml = tableSelect.outerHTML.replace(/ /g, '%20');
+      let downloadLink = document.createElement('a');
+      document.body.appendChild(downloadLink);
+      downloadLink.href = 'data:' + dataType + ', ' + tableHtml;
+      downloadLink.download = 'server-report.xls';
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    } else {
+      console.warn('Table with ID "servers" not found.');
+    }
+  }
 }
